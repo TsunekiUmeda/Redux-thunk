d8965dbf62288035c4288f36a3d6558bb4f49844
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-01 21:27:34 +0900

    Apply redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
-export const fetchPost = () => {
-  return {
-    type: 'FETCH_POST',
-  }
+import jsonPlaceHolder from '../apis/jsonPlaceHolder'
+
+export const fetchPost = () => async dispatch => {
+  const response = await jsonPlaceHolder.get('/post')
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
 }
