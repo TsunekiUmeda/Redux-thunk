042a46bf1f41422b48f5138a2f4d12a7d319b3f9
Tsuneki Umeda <umedat@amazon.co.jp>
2020-10-04 22:53:08 +0900

    Implement UsersReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,5 @@ export const fetchPost = () => async dispatch => {
 
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceHolder.get(`/users/${id}`)
-
   dispatch({ type: 'FETCH_USER', payload: response.data })
 }
